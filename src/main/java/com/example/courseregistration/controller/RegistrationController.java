ce817f9a5d765cdf1218bb4f53f86a805a110c01
IT25100913 <padrtfuka!@132>
2026-05-20 17:18:24 +0530

    controller editing

--- src/main/java/com/example/courseregistration/controller/RegistrationController.java
+++ src/main/java/com/example/courseregistration/controller/RegistrationController.java
@@ -17,7 +17,7 @@ public class RegistrationController {
     private final StudentService studentService;
     private final CourseService courseService;
 
-    @GetMapping
+    @GetMapping//getting values from server
     public String listRegistrations(Model model) {
         model.addAttribute("registrations", registrationService.getAllRegistrations());
         return "registrations/list";
